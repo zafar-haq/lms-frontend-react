5c38cd5f1c6df9173d18546d6bcc19e5c15b14d4
Zafar Ul Haq <zafar.haq@invozone.com>
2022-01-09 14:41:55 +0500

    admin view students crud

--- src/components/admin/AdminViewStudents.js
+++ src/components/admin/AdminViewStudents.js
@@ -1,20 +1,45 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { DataGrid } from "@mui/x-data-grid"
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import axiosService from "../../services/axiosService";
+
 function AdminViewStudents() {
+
+    const [open, setOpen] = useState(false);
+    const [credentials, setCredentials] = useState({ email: '', name: '', password: '' })
 
     const viewStudents = useSelector((state) => state.adminViewStudents)
     const dispatch = useDispatch()
     useEffect(() => {
-        const payload = { token: localStorage.getItem('adminToken') }
-        dispatch({ type: 'ADMIN_VIEW_STUDENTS_REQUEST', payload: payload })
+        dispatch({ type: 'ADMIN_VIEW_STUDENTS_REQUEST', payload: { token: localStorage.getItem('adminToken') } })
     }, [])
+
+    async function createStudent() {
+        const { name, email, password } = credentials
+        const payload = { email: email, name: name, password: password }
+        const token = localStorage.getItem('adminToken')
+        try {
+            const response = await axiosService.send('admin/create/student', token, payload, 'post')
+            dispatch({ type: 'ADMIN_VIEW_STUDENTS_REQUEST', payload: { token: token } })
+        } catch (e) {
+            console.log(e.response)
+        }
+        setOpen(false)
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <Box sx={{ display: 'inline-flex', flexDirection: 'row' }}>
@@ -33,13 +58,56 @@ function AdminViewStudents() {
                 />
             </Box>
             <Stack>
-                <Button variant="contained" color="success" startIcon={<AddIcon />} sx={{ mt: 5, ml: 2 }} >
+                <Button variant="contained" color="success" startIcon={<AddIcon />} sx={{ mt: 5, ml: 2 }} onClick={handleOpen} >
                     Create Student
                 </Button>
                 <Button variant="outlined" color="error" startIcon={<RemoveIcon />} sx={{ mt: 1, ml: 2 }} >
                     Remove Student
                 </Button>
             </Stack>
+
+            {/* dialog box  */}
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Create new Student</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Create Student credentials.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="email"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => { setCredentials({ ...credentials, email: e.target.value }) }}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="name"
+                        label="Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => { setCredentials({ ...credentials, name: e.target.value }) }}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="password"
+                        label="Password"
+                        type="password"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => { setCredentials({ ...credentials, password: e.target.value }) }}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={createStudent}>Create</Button>
+                </DialogActions>
+            </Dialog>
+
         </Box >
 
     )
